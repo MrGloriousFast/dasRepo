5c79b353d424528f96ade19e5457d960ef8e593d
MrGLoriousFast <schirmcharmemelone@gmail.com>
2017-06-03 00:00:12 +0200

    black window

---
+++ main.py
@@ -0,0 +1,50 @@
+import pygame,math
+import random
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+
+def main():
+	pygame.init()
+	display = (800,600)
+	pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+
+	#fov,aspectratio,znear,zfar
+	gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
+
+	glTranslatef(0.0,0.0, -10)
+
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				quit()
+			
+			#keybuttons move the view
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_LEFT:
+					glTranslatef(-0.1,0,0)
+
+				if event.key == pygame.K_RIGHT:
+					glTranslatef(0.1,0,0)
+
+				if event.key == pygame.K_UP:
+					glTranslatef(0,0.1,0)
+
+				if event.key == pygame.K_DOWN:
+					glTranslatef(0,-0.1,0)
+
+			if event.type == pygame.MOUSEBUTTONDOWN:
+				if event.button == 4:
+					glTranslatef(0,0,0.1)
+				if event.button == 5:
+					glTranslatef(0,0,-0.1)
+
+
+		#glRotatef(1, 3, 1, 1)
+		glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+		pygame.display.flip()
+		pygame.time.wait(10)
+
+main()
