43f1070c40baa478d6ec91140c417a195f6e7627
MrGLoriousFast <schirmcharmemelone@gmail.com>
2017-06-03 03:05:40 +0200

    space instead of tabs

--- main.py
+++ main.py
@@ -7,53 +7,53 @@ import obj
 import camera
 
 def main():
-	pygame.display.init()
-	display = (800,600)
-	pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+    pygame.display.init()
+    display = (800,600)
+    pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
 
-	#Create the camera
-	cam = camera.Camera(45, (display[0]/display[1]), 0.1, 50.0)
+    #Create the camera
+    cam = camera.Camera(45, (display[0]/display[1]), 0.1, 50.0)
 
-	#create a cube
-	cube = obj.Cube(1.0,(0,0,0))
+    #create a cube
+    cube = obj.Cube(1.0,(0,0,0))
 
-	glTranslatef(0.0,0.0, -10)
+    glTranslatef(0.0,0.0, -10)
 
-	while True:
-	    
-		#keyboard down presses
-		pressed = pygame.key.get_pressed()
-	
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				pygame.quit()
-				quit()
-			
-			if event.type == pygame.MOUSEBUTTONDOWN:
-				if event.button == 4:
-					glTranslatef(0,0,0.1)
-				if event.button == 5:
-					glTranslatef(0,0,-0.1)
+    while True:
+        
+        #keyboard down presses
+        pressed = pygame.key.get_pressed()
+    
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 4:
+                    glTranslatef(0,0,0.1)
+                if event.button == 5:
+                    glTranslatef(0,0,-0.1)
 
-		
-		if pressed[pygame.K_a]:
-			cam.moveLEFT()
+        
+        if pressed[pygame.K_a]:
+            cam.moveLEFT()
 
-		if pressed[pygame.K_d]:
-			cam.moveRIGHT()
-			
-		if pressed[pygame.K_w]:
-			cam.moveUP()
+        if pressed[pygame.K_d]:
+            cam.moveRIGHT()
+            
+        if pressed[pygame.K_w]:
+            cam.moveUP()
 
-		if pressed[pygame.K_s]:
-			cam.moveDOWN()
-			
-				
+        if pressed[pygame.K_s]:
+            cam.moveDOWN()
+            
+                
 
-		#glRotatef(1, 3, 1, 1)
-		glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-		cube.render()
-		pygame.display.flip()
-		pygame.time.wait(10)
+        #glRotatef(1, 3, 1, 1)
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+        cube.render()
+        pygame.display.flip()
+        pygame.time.wait(10)
 
 main()
