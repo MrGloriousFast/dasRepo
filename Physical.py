62c311eafaa10d59337ca6871f04779ce4a89c8d
Tom F. Buchholz <tom.friedrich.buchholz@gmail.com>
2017-06-03 04:56:50 +0200

    Add physical template. Forgot to use force commit in last message

---
+++ Physical.py
@@ -0,0 +1,11 @@
+class Physical:
+    """
+    Physicals enrich objects with:
+        direction [ * normal direction_x
+                    * normal direction y
+                    * normal direction z
+                    * speed scalar
+                  ]
+    """
+    
+
